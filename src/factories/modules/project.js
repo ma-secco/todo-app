deb676eba5a81d2c9c523da79e8b10685d78a206
Maria Secco <mapradosecco@gmail.com>
2024-03-14 17:35:57 -0300

    .

--- src/factories/modules/project.js
+++
@@ -1,8 +0,0 @@
-export default class Project {
-    constructor(name, id, todoList = [], todoUniqueID = 0) {
-      this.name = name;
-      this.id = id;
-      this.todoList = todoList;
-      this.todoUniqueID = todoUniqueID;
-    }
-  }
