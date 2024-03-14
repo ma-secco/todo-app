4a3668462369944fa2fc7ccb0116889eb762ad8c
Maria Secco <mapradosecco@gmail.com>
2024-03-14 16:05:24 -0300

    sccs correções

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
+        include: path.resolve(__dirname, 'src'), // Novo padrão de correspondência para a pasta styles
         use: [
           // Adiciona as folhas de estilo ao DOM injetando a tag <style>
           'style-loader',
